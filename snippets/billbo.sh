e125f8c07e7ba84639fe416511b3ac44a55a8c0a
Bill Flynn <wflynny@gmail.com>
2020-09-18 18:44:01 -0400

    Updated billbo

--- snippets/billbo.sh
+++ snippets/billbo.sh
@@ -10,3 +10,21 @@ alias vim=nvim
 sudo mkdir /opt/work && sudo chgrp jaxuser /opt/work && sudo chmod g+w /opt/work && cd /opt/work
 miniconda_url="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
 wget -O miniconda.sh $miniconda_url && bash miniconda.sh -b -s -p /opt/work/tools/miniconda
+
+mkdir -p /opt/work/logs /opt/work/configs/.secrets
+cat << 'EOF' > /opt/work/configs/sourceme.bash
+## >>> conda initialize >>>
+## !! Contents within this block are managed by 'conda init' !!
+#__conda_setup="$('/opt/miniconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#if [ $? -eq 0 ]; then
+#    eval "$__conda_setup"
+#else
+#    if [ -f "/opt/miniconda/etc/profile.d/conda.sh" ]; then
+#        . "/opt/miniconda/etc/profile.d/conda.sh"
+#    else
+#        export PATH="/opt/miniconda/bin:$PATH"
+#    fi
+#fi
+#unset __conda_setup
+## <<< conda initialize <<<
+EOF
